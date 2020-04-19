76cddc6625ba57dafe4ddaaa0c1e073d62e20792
AlaaElden98 <34111697+AlaaElden98@users.noreply.github.com>
2020-04-19 05:23:54 +0200

    Create NoteList Activity and Note Activity. Create DataBase and Create DataManger singelton. Temporarily design the xml for Note and NoteList Activities.

---
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -0,0 +1,84 @@
+package com.example.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.List;
+
+public class NoteActivity extends AppCompatActivity {
+    public static final String NOTE_POSITION ="com.example.notekeeper.NOTE_POSITION";
+    public static final int POSITION_NOT_SET = -1;
+    private NoteInfo mNote;
+    private boolean mIsNewNote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
+        List<CourseInfo> courses=DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinnerCourses.setAdapter(adapterCourses);
+
+        readDisplayStateValues();
+
+        EditText textNoteTitle = findViewById(R.id.text_note_title);
+        EditText textNoteText = findViewById(R.id.text_note_text);
+
+        if(!mIsNewNote)
+        displayNote(spinnerCourses,textNoteTitle,textNoteText);
+
+    }
+
+    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+        List<CourseInfo>courses = DataManager.getInstance().getCourses();
+        int courseIndex = courses.indexOf(mNote.getCourse());
+        spinnerCourses.setSelection(courseIndex);
+        textNoteTitle.setText(mNote.getTitle());
+        textNoteText.setText(mNote.getText());
+    }
+
+    private void readDisplayStateValues() {
+        Intent intent =getIntent();
+        int position =intent.getIntExtra(NOTE_POSITION,POSITION_NOT_SET)    ;
+        mIsNewNote = position==POSITION_NOT_SET;
+        if(!mIsNewNote){
+            mNote=DataManager.getInstance().getNotes().get(position);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
