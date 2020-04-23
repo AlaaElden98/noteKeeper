d216716e070a645299475f1b16857d43e99d3c73
AlaaElden98 <34111697+AlaaElden98@users.noreply.github.com>
2020-04-23 06:52:31 +0200

    Update ListView after press back button

--- app/src/main/java/com/example/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -17,6 +17,8 @@ import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
 
+    private ArrayAdapter<NoteInfo> mAdapterNotes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +37,19 @@ public class NoteListActivity extends AppCompatActivity {
         initializeDisplayContent();
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapterNotes.notifyDataSetChanged();
+    }
+
     private void initializeDisplayContent() {
         final ListView listNotes = findViewById(R.id.list_notes);
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        ArrayAdapter<NoteInfo> adapterNotes =
-                new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,notes);
+        mAdapterNotes = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,notes);
 
-        listNotes.setAdapter(adapterNotes);
+        listNotes.setAdapter(mAdapterNotes);
 
         listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
